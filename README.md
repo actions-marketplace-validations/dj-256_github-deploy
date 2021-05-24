4dafbadf8b63647308406528398a19082667dc04
Joel <joel@dibasso.fr>
2021-05-24 20:37:58 +0200

    Refactor and edit README
    * abandon promises
    * new function getPaths that returns the list of the paths to copy
    * edit README
    * add new tests

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
 # github-deploy
 Github Action providing simple scp feature
+
+## Examples
+
+### **Copy a single file**
+
+```yml
+- name: Copy single file
+  uses: dj-256/github-deploy@v1
+  with:
+    local: main.py
+    remote: /home/user/example
+    host: ${{ secrets.HOST }}
+    port: ${{ secrets.PORT }}
+    username: ${{ secrets.USERNAME }}
+    password: ${{ secrets.PASSWORD }}
+
+```
+
+### **Copy a directory recursively**
+
+```yml
+- name: Copy entire directory
+  uses: dj-256/github-deploy@v1
+  with:
+    local: ./dist
+    remote: /var/www/app
+    host: ${{ secrets.HOST }}
+    username: ${{ secrets.SSH_USER }}
+    password: ${{ secrets.PASSWORD }}
+
+```
+
+### **Copy a directory excluding some files**
+```yml
+- name: Excluding JS files
+  uses: dj-256/github-deploy@v1
+  with:
+      local: dist
+      remote: /home/example/
+      host: ${{ secrets.HOST }}
+      username: ${{ secrets.SSH_USER }}
+      password: ${{ secrets.PASSWORD }}
+      exclude: '*.js'
+```
+
+### **Copy a directory excluding a subdirectory**
+```yml
+- name: Copy exluding subdirectory
+    uses: dj-256/github-deploy@v1
+    with:
+      local: ./
+      remote: /var/www/dir
+      host: ${{ secrets.HOST }}
+      username: ${{ secrets.SSH_USER }}
+      password: ${{ secrets.PASSWORD }}
+      port: ${{ secrets.SSH_PORT }}
+      exclude: 'out/*'
+```
+
+
+## Options
+
+- **local** - _string_ - Path to the local file or directory you want to copy. **required**
+
+- **remote** - _string_ - Path to the remote directory to copy the contents to. **required**
+
+- **host** - _string_ - Hostname or IP address of the server. **Default:** `'localhost'`
+
+- **port** - _integer_ - Port number of the server. **Default:** `22`
+
+- **username** - _string_ - Username for authentication. **Default:** (none)
+
+- **password** - _string_ - Password for password-based user authentication. **Default:** (none)
+
+- **passphrase** - _string_ - For an encrypted private key, this is the passphrase used to decrypt it. **Default:** (none)
+
+- **rmRemote** - _boolean_ - Clean directory before uploading. **Default:** `false`
+
+- **exclude** - _string_ - Paths excluded from the copy (relative to remote path)
+
+## Development
+
+This has been strongly inspired by [https://github.com/garygrossgarten/github-action-scp](garygrossgarten's github-action-scp)
+it uses [https://www.npmjs.com/package/node-scp](node-scp) and [https://www.npmjs.com/package/glob](glob)
--- src/utilities.js
+++ src/utilities.js
@@ -3,88 +3,107 @@ const scp = require('node-scp')
 const fs = require('fs')
 const glob = require('glob')
 const path = require('path')
+const {error} = require("@actions/core");
+let client
+let base
+let errorCount = 0
+let errorFiles = []
+let r
 
-copy = (
+async function uploadFile (local, remote) {
+    let remotePath = remote + local.replace(r, "$2")
+    try {
+        await client.uploadFile(local, remotePath).then(()=>
+        console.log(`copied file ${local} to ${remotePath} 🟢`))
+    } catch (e) {
+        console.log(`couldn't copy file ${local} to ${remotePath} 🔴`)
+        console.log(e)
+        // errorCount++;
+        // errorFiles.push(local)
+    }
+}
+
+async function uploadDir (local, remote) {
+    let remotePath = remote + local.replace(r, "$2")
+    try {
+        await client.uploadDir(local, remotePath)
+        console.log(`copied directory ${local} to ${remotePath} 🟢`)
+    } catch (e) {
+        console.log(`couldn't copy directory ${local} to ${remotePath} 🔴`)
+        console.log(e)
+        // errorCount++;
+    }
+}
+
+function getPaths (local) {
+    let paths = []
+
+    if (local.match(/\*/)) { // glob path
+        glob(local, (err, files) => {
+            files.forEach(file => {
+                // console.log(file)
+                paths.push(file)
+            })
+        })
+    } else if (fs.statSync(local).isFile()) { // single file
+        paths.push(local)
+    } else { // directory (most likely)
+        local = local.endsWith('/') ? local : local+'/'
+        let files = fs.readdirSync(local)
+        if (files.length===0) {
+            error('Directory is empty')
+        }
+        files.forEach(file =>  {
+            // console.log(file)
+            paths.push(local+file)
+        })
+    }
+    return paths
+}
+
+async function copy (
     host,
     port,
     username,
     password,
     local,
-    remote) =>
-{
-    // let errorCount = 0
-    // let errorFiles = []
-
-    scp({
-        host: host,
-        port: port,
-        username: username,
-        password: password,
-        // privateKey: privateKey,
-        // passphrase: passphrase,
-    }).then(client => {
-
-        const uploadFile = (local, remote) => {
-            let remotePath = remote + path.basename(local)
-            client.uploadFile(local, remotePath)
-                .then(response => {
-                    console.log(`copied file ${local} to ${remotePath} 🟢`)
-                    client.close()
-                })
-                .catch(e => {
-                    console.log(`couldn't copy file ${local} to ${remotePath} 🔴`)
-                    console.log(e)
-                    // errorCount++;
-                    // errorFiles.push(local)
-                })
-        }
-
-        const uploadDir = (local, remote) => {
-            let remotePath = remote + path.basename(local)
-            client.uploadDir(local, remotePath)
-                .then(response => {
-                    console.log(`copied directory ${local} to ${remotePath} 🟢`)
-                    client.close()
-                })
-                .catch(e => {
-                    console.log(`couldn't copy directory ${local} to ${remotePath} 🔴`)
-                    console.log(e)
-                    // errorCount++;
-                })
-        }
-
-        if (local.match(/\*/)) {
-            glob(local, (err, files) => {
-                files.forEach(file => {
-                    console.log(file)
-                    uploadFile(file, remote)
-                })
-            })
-        } else if (fs.statSync(local).isFile()) {
-            uploadFile(local, remote)
-        } else {
-            local = local.endsWith('/') ? local : local+'/'
-            fs.readdirSync(local).forEach(file =>  {
-                if (fs.statSync(local+file).isFile()) {
-                    uploadFile(local + file, remote)
-                } else {
-                    uploadDir(local+file, remote)
-                }
-            })
-        }
-        // if (errorCount===0) {
-        //     console.log('All files copied successfully! 💯')
-        // } else {
-        //     console.log(`Some files haven't been copied :`)
-        //     errorFiles.forEach(file => console.log('\n'+file))
-        // }
-    }).catch(e => {
+    remote
+) {
+    try {
+        client = await scp({
+            host: host,
+            port: port,
+            username: username,
+            password: password,
+            // privateKey: privateKey,
+            // passphrase: passphrase
+        })
+    } catch (e) {
         console.log(`Couldn't connect to server ❌\nPlease check your action parameters`)
         console.log(e)
-    })
+        process.exit(1)
+    }
+
+    base = path.basename(local)
+    r = RegExp(`.*(\.\/)?${base}\/(.*)`)
+    let stack = getPaths(local)
+    while (stack.length!==0) {
+        let path = stack.pop()
+        if (fs.statSync(path).isFile()) {
+            await uploadFile(path, remote)
+            // console.log(`file: ${path}`)
+        } else {
+            let paths = getPaths(path)
+            await uploadDir(path, remote)
+            // console.log(`directory: ${path}`)
+            paths.forEach(p => stack.push(p))
+        }
+    }
+    await client.close()
 }
 
-run = () => {
+async function run (
+) {
     const host = core.getInput('host');
     const username = core.getInput('username');
     const port = +core.getInput('port') || 22;
@@ -102,7 +121,8 @@ run = () => {
     // const atomicPut = core.getInput('atomicPut');
     // const exclude = core.getInput('exclude') || null;
 
-    copy(
+
+    await copy(
         host,
         port,
         username,
@@ -112,7 +132,5 @@ run = () => {
     )
 }
 
-module.exports = {
-    copy: copy,
-    run: run,
-}
+module.exports = { copy, run }
+
