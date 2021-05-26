bc63f5c5685565b8bd05a1fb75420da09872f601
Joel <joel@dibasso.fr>
2021-05-26 16:39:47 +0200

    missing files

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var utilities_1 = require("./utilities");
+utilities_1.run().then(function () { return process.exit(0); });
