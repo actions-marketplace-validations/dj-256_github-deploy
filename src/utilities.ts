a1d72ea16364ea3e7d1a6a2f09ced18f0d0ae29c
Joel <joel@dibasso.fr>
2022-03-26 03:27:09 +0100

    Trying to fix rmRemote default value

--- src/utilities.ts
+++ src/utilities.ts
@@ -214,10 +214,10 @@ export async function run (
             password: core.getInput('password'),
             port: +core.getInput('port') || 22,
             local: core.getInput('local'),
-            dotFiles: !!core.getInput('dotfiles'),
+            dotFiles: !!core.getInput('dotfiles') || true,
             remote: core.getInput('remote'),
             exclude: core.getInput('exclude') || '',
-            rmRemote: !!core.getInput('rmRemote'),
+            rmRemote: !!core.getInput('rmRemote') || false,
         })
     }
 }
