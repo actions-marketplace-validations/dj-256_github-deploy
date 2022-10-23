c25879907dae482e7e4327f9473be74caab410af
Joel <joel@dibasso.fr>
2022-10-23 21:39:33 +0200

    Add possibility to deploy with ssh key

---
+++ src/options.ts
@@ -0,0 +1,13 @@
+export interface Options {
+    host: string,
+    username: string,
+    password: string,
+    privateKey: string,
+    passphrase: string,
+    port: number,
+    local: string,
+    remote: string,
+    exclude: string,
+    dotFiles: boolean,
+    rmRemote: boolean,
+}
--- src/utilities.ts
+++ src/utilities.ts
@@ -1,9 +1,10 @@
 import * as core from "@actions/core"
+import config from "./config"
+import { Options } from "./options"
 import path = require("path")
 import scp = require("node-scp")
 import fs = require("fs")
 import glob = require("glob")
-import ini = require("ini")
 
 let errors = 0
 let client: scp.ScpClient
@@ -17,7 +18,7 @@ class Element {
     type: PathType
     path: string
 
-    constructor(type:PathType, path:string) {
+    constructor(type: PathType, path: string) {
         this.type = type
         this.path = path
     }
@@ -29,21 +30,9 @@ interface NodeSCPElement {
     size?: number
     modifyTime: number
     accessTime: number
-    rights: {user: string, group: string, other: string}
+    rights: { user: string, group: string, other: string }
     owner: number
-    group:number
-}
-
-interface Options {
-    host: string,
-    username: string,
-    password: string,
-    port: number,
-    local: string,
-    remote: string,
-    exclude: string,
-    dotFiles: boolean,
-    rmRemote: boolean,
+    group: number
 }
 
 async function removeRemoteFile(file: string) {
@@ -67,7 +56,7 @@ async function removeRemoteDir(dir: string) {
 }
 
 async function removeRemoteElement(element: NodeSCPElement, remote: string) {
-    if (element.type==='d') {
+    if (element.type === "d") {
         return await removeRemoteDir(path.join(remote, element.name))
     } else {
         return await removeRemoteFile(path.join(remote, element.name))
@@ -81,10 +70,10 @@ async function removeRemote(remote: string) {
     } catch (err: any) {
         console.log(err.message)
     }
-    for(let file of files) {
+    for (let file of files) {
         await removeRemoteElement(file, remote)
     }
-    console.log('Cleared remote\n')
+    console.log("Cleared remote\n")
 }
 
 const relativeLocal = (filePath: string) => {
@@ -94,11 +83,11 @@ const relativeLocal = (filePath: string) => {
     return filePath.substring(localBase.length)
 }
 
-async function upload (element: Element, remote: string) {
+async function upload(element: Element, remote: string) {
     let localPath = element.path
     let remotePath = path.join(remote, relativeLocal(localPath))
 
-    if (element.type===PathType.file) {
+    if (element.type === PathType.file) {
         try {
             await client.uploadFile(localPath, remotePath)
             console.log(`🟢 Copied file ${localPath} to ${remotePath}`)
@@ -130,20 +119,20 @@ function getElements(local: string, exclude: string, dotFiles: boolean) {
     if (fs.statSync(local).isFile()) {
         return [{type: PathType.file, path: local}]
     }
-    if (exclude!=='') {
+    if (exclude !== "") {
         exclude = path.join(local, exclude)
         if (!exclude.match(/\*/)) {
-            excludeGlob = path.join(exclude, '/**/*')
+            excludeGlob = path.join(exclude, "/**/*")
         } else {
             excludeGlob = exclude.replace(/\*\.(\w*)$/, "**/*.$1")
         }
         glob.sync(excludeGlob).forEach(file => excludeList.push(file))
         excludeList.push(exclude)
     }
-    glob.sync('.git/**/*').forEach(file => excludeList.push(file))
-    glob.sync('.github/**/*').forEach(file => excludeList.push(file))
-    glob.sync('.idea/**/*').forEach(file => excludeList.push(file))
-    files = glob.sync(path.join(local, '**/*'), {ignore:excludeList, dot:dotFiles})
+    glob.sync(".git/**/*").forEach(file => excludeList.push(file))
+    glob.sync(".github/**/*").forEach(file => excludeList.push(file))
+    glob.sync(".idea/**/*").forEach(file => excludeList.push(file))
+    files = glob.sync(path.join(local, "**/*"), {ignore: excludeList, dot: dotFiles})
     files.forEach(localPath => {
         if (fs.statSync(localPath).isFile()) {
             elements.push({type: PathType.file, path: localPath})
@@ -151,28 +140,35 @@ function getElements(local: string, exclude: string, dotFiles: boolean) {
             elements.push({type: PathType.directory, path: localPath})
         }
     })
-    console.log('Element list generated\n')
+    console.log("Element list generated\n")
     return elements
 }
 
-export async function copy (
-    {host,
-    port,
-    username,
-    password,
-    local,
-    remote,
-    exclude,
-    dotFiles,
-    rmRemote}: Options
+export async function copy(
+    {
+        host,
+        port,
+        username,
+        password,
+        privateKey,
+        passphrase,
+        local,
+        remote,
+        exclude,
+        dotFiles,
+        rmRemote
+    }: Options
 ) {
     try {
         // @ts-ignore
         client = await scp({
-            host: host,
-            port: port,
-            username: username,
-            password: password,
+            host,
+            port,
+            username,
+            password,
+            privateKey,
+            passphrase
+
         })
     } catch (e: any) {
         console.error(`Couldn't connect to server ❌\nPlease check your action parameters: ${e.message}`)
@@ -184,41 +180,40 @@ export async function copy (
     remote = path.normalize(remote)
 
     if (rmRemote) {
-        console.log('Clearing remote...')
+        console.log("Clearing remote...")
         await removeRemote(remote)
     }
-    console.log('Generating element list...')
+    console.log("Generating element list...")
     let elements = getElements(local, exclude, dotFiles)
-    console.log('Uploading...')
+    console.log("Uploading...")
     for (const elt of elements) {
         await upload(elt, remote)
     }
     await client.close()
     if (errors > 0) {
-        console.error(`\n\n${errors} error${errors > 1 ? 's' : ''}`)
+        console.error(`\n\n${errors} error${errors > 1 ? "s" : ""}`)
         process.exit(1)
     } else {
         console.log("\n\nAll files transferred successfully")
     }
 }
 
-export async function run (
-) {
-    if (process.env.GITHUB_ACTION_ENV === 'test') {
-        let config = ini.parse(fs.readFileSync('configuration.ini', 'utf-8'));
-        let options = config.options
-        await copy(options)
+export async function run() {
+    if (process.env.GITHUB_ACTION_ENV === "test") {
+        await copy(config)
     } else {
         return await copy({
-            host: core.getInput('host'),
-            username: core.getInput('username'),
-            password: core.getInput('password'),
-            port: +core.getInput('port') || 22,
-            local: core.getInput('local'),
-            dotFiles: core.getInput('dotfiles') === "" ? true : core.getInput('dotfiles') === "true",
-            remote: core.getInput('remote'),
-            exclude: core.getInput('exclude') || '',
-            rmRemote: core.getInput('rmRemote') === "" ? false : core.getInput('rmRemote') === "true",
+            host: core.getInput("host"),
+            username: core.getInput("username"),
+            password: core.getInput("password"),
+            privateKey: core.getInput("privateKey"),
+            passphrase: core.getInput("passphrase"),
+            port: +core.getInput("port") || 22,
+            local: core.getInput("local"),
+            dotFiles: core.getInput("dotfiles") === "" || core.getInput("dotfiles") === "true",
+            remote: core.getInput("remote"),
+            exclude: core.getInput("exclude"),
+            rmRemote: core.getInput("rmRemote") === "true",
         })
     }
 }
